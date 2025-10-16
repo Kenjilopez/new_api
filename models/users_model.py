f9a48009fbb37ae543a637b90800c8374b958f08
Kenjilopez <kjlopezf@academia.usbbog.edu.co>
2025-10-16 14:49:54 -0500

    edit database & adding role and email

--- models/users_model.py
+++ models/users_model.py
@@ -13,9 +13,9 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     # Define la columna 'username' como una cadena de hasta 50 caracteres
     # Asegura que cada nombre de usuario sea único, lo indexa para búsquedas rápidas y no permite valores nulos
-    username = Column(String(50), unique=True, index=True, nullable=False)
+    email = Column(String(50), unique=True, index=True, nullable=False)
     # Define la columna 'password' como una cadena de hasta 255 caracteres
     # No permite valores nulos. La longitud sugiere que se almacenará un hash de la contraseña, no la contraseña en texto plano
     password = Column(String(255), nullable=False)
-
-
+    
+    role = Column(String(50, nullable=False),default='user')
