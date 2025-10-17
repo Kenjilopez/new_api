8c51999ffc58e92f1e39ece51fbb1a715451406f
Kenjilopez <kjlopezf@academia.usbbog.edu.co>
2025-10-17 14:40:37 -0500

    adjusting interations with admin inside 'car_controllers{

--- controllers/car_controller.py
+++ controllers/car_controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from repository.car_repository import Car, CarStore, Base
@@ -10,8 +11,20 @@ engine = create_engine('sqlite:///cars.db')
 Session = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
 
+
+def admin_required():
+    # identity is the subject (string id). Read additional claims with get_jwt()
+    claims = get_jwt()
+    if claims.get('role') != 'admin':
+        return jsonify({'error': 'Acceso denegado: solo administradores'}), 403
+ 
+ 
 @car_store_bp.route('/cars', methods=['POST'])
+@jwt_required()
 def add_car():
+    auth = admin_required()
+    if auth: return auth
+    
     data = request.get_json()
     model = data.get('model')
     store_id = data.get('store_id')
@@ -26,7 +39,10 @@ def add_car():
     return jsonify({'message': 'Carro insertado correctamente'}), 201
 
 @car_store_bp.route('/cars', methods=['GET'])
+@jwt_required()
 def get_cars():
+    auth = admin_required()
+    if auth: return auth
     session = Session()
     cars = session.query(Car).all()
     result = [
@@ -37,7 +53,10 @@ def get_cars():
     return jsonify(result), 200
 
 @car_store_bp.route('/cars/<int:car_id>', methods=['PUT'])
+@jwt_required()    
 def update_car(car_id):
+    auth = admin_required()
+    if auth: return auth
     data = request.get_json()
     session = Session()
     car = session.query(Car).get(car_id)
@@ -51,7 +70,10 @@ def update_car(car_id):
     return jsonify({'message': 'Carro actualizado'}), 200
 
 @car_store_bp.route('/cars/<int:car_id>', methods=['DELETE'])
+@jwt_required()
 def delete_car(car_id):
+    auth = admin_required()
+    if auth: return auth
     session = Session()
     car = session.query(Car).get(car_id)
     if not car:
